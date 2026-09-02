a58eb7b0c2d99581f11b8a68def8170eca2215b1
Miles Conrad <72412676+milesconrad@users.noreply.github.com>
2026-09-02 12:49:09 -0400

    Swapped global boolean for a thread event

--- autoclik.py
+++ autoclik.py
@@ -1,14 +1,13 @@
 from pynput.mouse import Controller, Button
 from pynput.keyboard import Listener, Key
-from threading import Thread
+from threading import Thread, Event
 from time import sleep
 
 mouse = Controller()
-active = False
+active = Event()
 last_key = None
 
 def on_press(pressed_key):
-    global active
     global last_key
     # if you try to call pressedKey.char when pressedKey is a key object, then
     # it throws an AttributeError and this thread closes
@@ -16,15 +15,18 @@ def on_press(pressed_key):
         if pressed_key.char == ']':
             if not isinstance(last_key, Key):
                 if last_key.char == '[':
-                    active = not active
-
+                    if active.is_set():
+                        active.clear()
+                    else:
+                        active.set()
+                        
     last_key = pressed_key
     
 def clicker():
     while True:
-        if active:
-            mouse.click(Button.left)
-            sleep(0.005)
+        active.wait()
+        mouse.click(Button.left)
+        sleep(0.005)
 
 clickerThread = Thread(target=clicker)
 listenerThread = Listener(on_press=on_press)
